1c253c88eeb731b4d72a516a56e8c95f8550dfde
Mert Akcakaya <mert@meltabi.com>
2019-10-29 13:50:56 +0300

    Initial commit after fork

---
+++ .sfdx/typings/lwc/sobjects/Log_Event__e.d.ts
@@ -0,0 +1,44 @@
+declare module "@salesforce/schema/Log_Event__e.Class__c" {
+  const Class__c:string;
+  export default Class__c;
+}
+declare module "@salesforce/schema/Log_Event__e.Context__c" {
+  const Context__c:string;
+  export default Context__c;
+}
+declare module "@salesforce/schema/Log_Event__e.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Log_Event__e.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Log_Event__e.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Log_Event__e.Level__c" {
+  const Level__c:string;
+  export default Level__c;
+}
+declare module "@salesforce/schema/Log_Event__e.Line_Number__c" {
+  const Line_Number__c:number;
+  export default Line_Number__c;
+}
+declare module "@salesforce/schema/Log_Event__e.Message__c" {
+  const Message__c:string;
+  export default Message__c;
+}
+declare module "@salesforce/schema/Log_Event__e.Method__c" {
+  const Method__c:string;
+  export default Method__c;
+}
+declare module "@salesforce/schema/Log_Event__e.ReplayId" {
+  const ReplayId:string;
+  export default ReplayId;
+}
+declare module "@salesforce/schema/Log_Event__e.User__c" {
+  const User__c:string;
+  export default User__c;
+}
